b696d2bf2cdd0a7d3c9496f3700f1e5c02f00a09
TOVTC <veronica.tc.to@outlook.com>
2022-07-14 15:19:39 -0400

    added mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,10 @@ const {gql} = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+    type Auth {
+        token: ID!
+        user: User
+    }
     type User {
         _id: ID
         username: String
@@ -25,11 +29,21 @@ const typeDefs = gql`
         createdAt: String
         username: String
     }
+
     type Query {
+        me: User
         users: [User]
         user(username: String): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addThought(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
     }
 `;
 
